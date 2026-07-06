0d16bc9f0241e3cedfe4da7a1bbbd771bae9fe04
KIZIRTO RAMADHANI JAMALI <kizitorj04@gmail.com>
2026-07-06 06:14:36 +0300

    fix ALLOWED_HOSTS for Render Deployment

--- core/settings.py
+++ core/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-12345')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'iaa-accommodation-system.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://iaa-accommodation-system.onrender.com']
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
