03a5d0f2ce762e27d66c4aee4b9c204b884bae98
KIZIRTO RAMADHANI JAMALI <kizitorj04@gmail.com>
2026-07-07 13:14:35 +0300

    fix duplicate settings

--- core/settings.py
+++ core/settings.py
@@ -41,22 +41,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&rq*lddf%@(cq$4e%^380!6azlt=&@@=+ao$=q#286(av3tt^%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
