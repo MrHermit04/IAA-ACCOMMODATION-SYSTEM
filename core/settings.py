70e5c0af2d6479c50ac2dde31319362913270cec
KIZIRTO RAMADHANI JAMALI <kizitorj04@gmail.com>
2026-07-07 17:42:20 +0300

    connect postgres permanently

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 from pathlib import Path
 
 import os
@@ -82,10 +83,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+        )
 }
 
 
